91f956b4b77bd301986ea96d4dd20af1154fbc38
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2022-11-27 16:31:53 -0300

    estilizando pagina inicial

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from "react-bootstrap";
 import '../styles/main.css';
 
 class Main extends Component {
   render() {
     return (
-      <div className='maincont'>
-      <h1 className="heading">
-        Olá pessoal!<span className="wave">👋🏻</span>
-      </h1>
-      <h1 className="heading-name">
-        Meu nome é
-      <strong className="main-name"> Pedro Steinmüller Pimentel</strong>
-      </h1>
-      <br/>
-      <h2>Desenvolvedor Web FullStack</h2>
-        <div className="image">
-          <img src="pedinn.jpeg" alt = "pedro" className="myphoto"></img>
-        </div>
-      </div>
-    );
+      <section className="banner">
+        <Container className="teste">
+          <Row className="aligh-items-center">
+            <Col>
+                <div className="infos">
+                  <span className="tagline">Bem-vindo(a) ao meu Portfolio!</span>
+                  <h1>Olá, meu nome é Pedro Steinmüller Pimentel <span className="wave">👋🏻</span></h1> 
+                    <p>Graduado e Mestre em Física</p>
+                    <p>Desenvolvedor Web Fullstack</p>
+                </div>
+            </Col>
+            <Col xs={12} md={6} xl={5}>
+            <div className="image">
+              <img src="pedinn.jpeg" alt = "pedro" className="myphoto"></img>
+            </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    )
   };
 }
 
