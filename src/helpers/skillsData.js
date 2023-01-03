35c98d809509cf666766c96240b6c863ea8ef403
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-01-03 10:26:57 -0300

    feat: criado página de habilidades e organizacao de arquivos

---
+++ src/helpers/skillsData.js
@@ -0,0 +1,55 @@
+import git from './skillsImages/git.svg';
+import github from './skillsImages/github.svg';
+import html from './skillsImages/html.svg';
+import css from './skillsImages/css.svg';
+import javascript from './skillsImages/javascript.svg';
+import react from './skillsImages/react.svg';
+import jest from './skillsImages/jest.svg';
+import docker from './skillsImages/docker.svg';
+import mysql from './skillsImages/mysql.svg';
+import nodejs from './skillsImages/nodejs.svg';
+
+const mySkills = [
+  {
+    name: 'GitHub',
+    image: github,
+  },
+  {
+    name: 'Git',
+    image: git,
+  },
+  {
+    name: 'HTML',
+    image: html,
+  },
+  {
+    name: 'CSS',
+    image: css,
+  },
+  {
+    name: 'Javascript',
+    image: javascript,
+  },
+  {
+    name: 'React',
+    image: react,
+  },
+  {
+    name: 'Docker',
+    image: docker,
+  },
+  {
+    name: 'Node-JS',
+    image: nodejs,
+  },
+  {
+    name: 'Jest',
+    image: jest,
+  },
+  {
+    name: 'MySQL',
+    image: mysql,
+  },
+
+];
+export default mySkills;
