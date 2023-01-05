edfad6f837ec65d72606046d6a0e874ecbad5f26
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-01-04 22:08:45 -0300

    feat: mudando css do header/navbar para outro estilo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import '../styles/header.css';
 
 function ColorSchemesExample() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Página Inicial</Navbar.Brand>
-          <Nav className="me-auto">
-           <Nav.Link href="/About">Sobre mim</Nav.Link>
-            <Nav.Link href="/Projects">Projetos</Nav.Link>
-            <Nav.Link href="/Skills">Habilidades</Nav.Link>
+          <Navbar.Brand href="/" className='colortext'>Página Inicial</Navbar.Brand>
+          <Nav className="me-auto" id="item">
+           <Nav.Link href="/About" className='colortext'>Sobre mim</Nav.Link>
+            <Nav.Link href="/Projects" className='colortext'>Projetos</Nav.Link>
+            <Nav.Link href="/Skills" className='colortext'>Habilidades</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
